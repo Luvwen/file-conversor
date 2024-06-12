db5c37bf59baa5ecbaa5ffdac272d05629f18e85
Santiago Baliño <santiagobalino@hotmail.com>
2024-06-12 20:58:25 -0300

    Create backend server starting point

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,16 @@
-console.log('start point');
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import router from './routes/home.route';
+
+const app = express();
+
+const port = process.env.PORT ?? 3000;
+
+app.use(cors());
+
+app.listen(port, () => {
+	console.log('server online on port: ' + port);
+});
+
+app.use('/', router);
